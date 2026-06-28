09ea683c2bfcc3fc60d9f25263cf30dc7839f8d2
Zals <rahmataprizal403@gmail.com>
2026-06-28 12:24:21 +0700

    Remove Monetag verification from layout.js
    Removed Monetag verification meta tag and script from layout.

--- app/layout.js
+++ app/layout.js
@@ -19,9 +19,6 @@ export default function RootLayout({ children }) {
       <head>
         {/* Meta Google Site Verification Baru */}
         <meta name="google-site-verification" content="2LXfski1y6RJdglQv40n7jsKu3Ww5wWj5sANTHm-pAI" />
-
-        {/* Meta Monetag Verification */}
-        <meta name="monetag" content="00151d67ee72043ef4ef8676be634839" />
         
         <link
           href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700;800&family=Space+Mono:wght@400;700&display=swap"
@@ -34,12 +31,8 @@ export default function RootLayout({ children }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6307870813026612"
           crossOrigin="anonymous"
         ></script>
-
-        {/* Monetag */}
-        <script src="https://quge5.com/88/tag.min.js" data-zone="254131" async data-cfasync="false"></script>
       </head>
       <body>{children}</body>
     </html>
   );
 }
-
